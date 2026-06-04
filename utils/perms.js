0ec0edd9c9db9cb2ab91d3540a9660ee45369c37
Thomas Schneider (thsc) <118460072+ThomasSchneider-94@users.noreply.github.com>
2026-06-04 14:19:50 +0200

    Add role name and format role list functions

--- utils/perms.js
+++
@@ -1,6 +0,0 @@
-export function formatPermission(perm) {
-    return perm
-        .replace(/([A-Z])/g, ' $1') // split words
-        .replace(/^./, str => str.toUpperCase()) // capitalize first
-        .trim();
-}
