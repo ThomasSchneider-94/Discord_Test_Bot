478af85a492923edfbd1c8bd64f7cb5fb6b52ab8
Thomas Schneider (thsc) <118460072+ThomasSchneider-94@users.noreply.github.com>
2026-06-24 13:23:34 +0200

    Create __manifest__.js for roll_jb module

---
+++ modules/roll_jb/__manifest__.js
@@ -0,0 +1,7 @@
+export default {
+    name: "roll_jb",
+    displayName: "Roll sound",
+    description: "Add roll sound to the jukebox",
+    extensionOf: { "roll_dice", "jukebox" },
+    autoInstall: true,
+}
